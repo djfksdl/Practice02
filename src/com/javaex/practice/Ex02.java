669d42be339726634113abee948e2e49af8cfc63
djfksdl <djfksdl@gmail.com>
2024-01-03 14:24:13 +0900

    switch문 변수 문제

--- src/com/javaex/practice/Ex02.java
+++ src/com/javaex/practice/Ex02.java
@@ -2,15 +2,16 @@ package com.javaex.practice;
 
 public class Ex02 {
 	/*다음중 switch 문에서 조건식에 넣을 수 있는 데이터 타입은?
-		  x	(1) boolean
-		  0 (2) char
-		  0	(3) byte
-		  0 (4) short
-		  0 (5) int 
-		  x (6) long
-		  x	(7) float
-		  x	(8) double
-	  	  0 (9) String 
+		  x	(1) boolean 논리형
+		  0 (2) char 문자형.정수형 자료
+		  0	(3) byte 정수형
+		  0 (4) short 정수형
+		  0 (5) int  정수형
+		  x (6) long 
+		  x	(7) float 실수형
+		  x	(8) double 실수형
+	  	  0 (9) String 문자열
+	  	  switch문 조건식에는 long을 제외한 정수형 자료형만 넣을 수 있다. char는 문자형이지만 정수형 자료형으로써 가능. String도 가능하다.
 	*/
 	
 }
